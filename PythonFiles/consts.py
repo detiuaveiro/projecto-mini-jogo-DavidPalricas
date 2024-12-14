1e5b4d7baaad8ead7789c91d26d6f562d700ec48
davidpalricas <davipalricas22@gmail.com>
2024-12-14 00:41:23 +0000

    consts updated

--- PythonFiles/consts.py
+++ PythonFiles/consts.py
@@ -1,15 +1,28 @@
+import pygame as pg
+
+
+# Game Event Constants
+PLAYER_DEATH_EVENT = pg.USEREVENT + 1
+TIMEOUT_EVENT = pg.USEREVENT + 2
+TIME_ALERT_EVENT = pg.USEREVENT + 3
+JUMP_EVENT = pg.USEREVENT + 4
+END_GAME_EVENT = pg.USEREVENT + 5
+ENEMY_DEATH_EVENT = pg.USEREVENT + 6
+QUIT_GAME_EVENT = pg.USEREVENT + 7 
+
 # Screen SIze Constants
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 277
 
-# The floor block constants use to create the game map
+# Map Constants flor block constants and peach sprite path
 FLOOR_TILE_WIDTH = 16
 FLOOR_TILE_HEIGHT = 19
 FLOOR_BLOCK = 0
 FLOOR_BLOCK_SPRITE_PATH = "../Assets/SpriteSheets/Map/floor_block.png"
+PEACH_SPRITE_PATH = "../Assets/SpriteSheets/Peach/peach.png"
 
 # Time Constants
-GAME_TIME = 300
+GAME_TIME = 93
 ALERT_TIME = 90
 TIMEOUT = 0
 GAME_SECOND = 1000
